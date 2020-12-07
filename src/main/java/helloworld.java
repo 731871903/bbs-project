cc052224eb228412d22bb66e1e3f117144dbf047
Leon <731871903@qq.com>
2020-12-07 20:44:04 +0800

    test

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -4,7 +4,7 @@ import javax.sound.midi.Soundbank;
  * @author Liweicheng
  * @create 2020/12/7 - 20:05
  */
-public class helloworld {
+public class helloworld {s
     public static void main(String[] args) {
         System.out.println("nibuhaohao");
     }
