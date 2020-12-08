6c4b5b72b59b086544a747d5c78a6ef5233b900a
WeiCheng <69063657+731871903@users.noreply.github.com>
2020-12-08 19:13:12 +0800

    Update helloworld.java

--- src/main/java/helloworld.java
+++ src/main/java/helloworld.java
@@ -4,7 +4,7 @@ import javax.sound.midi.Soundbank;
  * @author Liweicheng
  * @create 2020/12/7 - 20:05
  */
-public class helloworld {s
+public class helloworld {
     public static void main(String[] args) {
         System.out.println("haohala");
     }
